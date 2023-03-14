4043e23cd4bf356fcc5fc9cf1b116e28770e4d7c
klozz <carlosj@klozz.dev>
2023-03-14 05:12:52 +0000

    kona: Correct the power hal we have 1.3 not 1.3-qti
    Signed-off-by: klozz <carlosj@klozz.dev>

--- kona.mk
+++ kona.mk
@@ -443,7 +443,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service \
     android.hardware.power@1.3.vendor
 
 # Public libraries
